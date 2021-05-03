9672101412b61a1add5348c35466979da8625944
azuregitter <geromeriego@gmail.com>
2021-05-03 08:32:55 +0800

    initial commit

---
+++ precache-manifest.2dd9303d1051efd2857aa43c198c4d1d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2c2ffede879e5c4264d658555032fe",
+    "url": "/it130project/index.html"
+  },
+  {
+    "revision": "8f01ee0e604dfbafb72c",
+    "url": "/it130project/static/css/main.2e04aaf3.chunk.css"
+  },
+  {
+    "revision": "4cc93a5931362394395b",
+    "url": "/it130project/static/js/2.72b3d6da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/it130project/static/js/2.72b3d6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f01ee0e604dfbafb72c",
+    "url": "/it130project/static/js/main.7f3f8348.chunk.js"
+  },
+  {
+    "revision": "c06c0d790023807f1101",
+    "url": "/it130project/static/js/runtime-main.3d17a68d.js"
+  },
+  {
+    "revision": "ae1e4bf6249d2b6e8a26fd3e431d40f2",
+    "url": "/it130project/static/media/0.ae1e4bf6.png"
+  },
+  {
+    "revision": "e0c1f78ff25aff823dfa714992588e99",
+    "url": "/it130project/static/media/1.e0c1f78f.png"
+  },
+  {
+    "revision": "71e71c54e0d003e704c612a4b2f2f0e6",
+    "url": "/it130project/static/media/3.71e71c54.png"
+  },
+  {
+    "revision": "d3a2ef6be27799ab1e447ffb391ec271",
+    "url": "/it130project/static/media/4.d3a2ef6b.png"
+  },
+  {
+    "revision": "1cccf7d47c7ce028ac41cb329a08853d",
+    "url": "/it130project/static/media/5.1cccf7d4.png"
+  },
+  {
+    "revision": "4ccbfbcaea654a50924b25fde44da7d3",
+    "url": "/it130project/static/media/6.4ccbfbca.png"
+  },
+  {
+    "revision": "077161b6fd3a763755bb57c99038c334",
+    "url": "/it130project/static/media/7.077161b6.png"
+  },
+  {
+    "revision": "1df7c80b89d3eda900e0b3be2f3a7258",
+    "url": "/it130project/static/media/8.1df7c80b.png"
+  },
+  {
+    "revision": "4c81cd865c6af3bc384555756516895c",
+    "url": "/it130project/static/media/asc-mo.4c81cd86.jpg"
+  },
+  {
+    "revision": "c00913ca61135ca35fa8a68a06a8dfb5",
+    "url": "/it130project/static/media/bannerdav16.c00913ca.jpg"
+  },
+  {
+    "revision": "b1e65fec9789ac980d05cb63a5f1093d",
+    "url": "/it130project/static/media/bannermindoro16.b1e65fec.jpg"
+  },
+  {
+    "revision": "044100476dc51df545711b833181075d",
+    "url": "/it130project/static/media/bowats.04410047.png"
+  },
+  {
+    "revision": "a37ccaa0048d89180840addb3f11fd96",
+    "url": "/it130project/static/media/g1.a37ccaa0.jpg"
+  },
+  {
+    "revision": "c8512739b6859b477fd38e41dbcf21b4",
+    "url": "/it130project/static/media/g10.c8512739.jpg"
+  },
+  {
+    "revision": "3513b38617e67e28c454c258bbb3cbb3",
+    "url": "/it130project/static/media/g2.3513b386.jpg"
+  },
+  {
+    "revision": "41c0ca4b59f60c7410b096b97a95e53f",
+    "url": "/it130project/static/media/g4.41c0ca4b.jpg"
+  },
+  {
+    "revision": "3101725ab6783a043f7d0344b7bb96ff",
+    "url": "/it130project/static/media/ship2.3101725a.gif"
+  },
+  {
+    "revision": "23afcf9de959af8b253c16b3019d0a76",
+    "url": "/it130project/static/media/title3.23afcf9d.png"
+  }
+]);
